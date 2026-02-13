91fd7f9bd4a40ff71ce3a7280d1577a93b936d0a
rohitdeveloper22 <118961542+rohitdeveloper22@users.noreply.github.com>
2026-02-13 10:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,11 +154,9 @@ def upload():
     })
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
